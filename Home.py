d25bec27efc6e142e8e4b66b963c01f7989afac5
celina <cxlina96@gmail.com>
2024-05-14 00:56:56 -0300

    Finished

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from functions import get_data
+from datetime import datetime
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input(label="Place", placeholder="")
@@ -8,9 +9,26 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecast days")
 option = st.selectbox(label="Select data to view: ",
                       options=('Temperature', 'Sky'))
-st.subheader(f"{option} for the next {days} days in {place}.")
+try:
+    if place:
+        data = get_data(place, days)
+        st.subheader(f"{option} for the next {days} days in {place}.")
+        dates = [dict['dt_txt'] for dict in data]
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in data]
+            figure = px.line(x=dates, y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-db = get_data(place, days, option)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            sky_conditions = [dict['weather'][0]['main'] for dict in data]
+            dates = [datetime.strptime(i, "%Y-%m-%d %H:%M:%S")
+                     for i in dates]
+            dates = [i.strftime("%a, %b %d %H:%M") for i in dates]
+            img_dict = {"Clear": "images/clear.png",
+                        "Clouds": "images/cloud.png",
+                        "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            img_path = [img_dict[sky] for sky in sky_conditions]
+            st.image(img_path, width=115, caption=dates)
+except KeyError:
+    st.info("Please enter a valid city. Thank you")
--- functions.py
+++ functions.py
@@ -1,28 +1,19 @@
 import requests
-import datetime
 
 API_KEY = "94dd38bf3fb5ab6310819a999d2bc8c2"
 
-def get_data(place, days, kind):
+def get_data(place, days):
     url = ("http://api.openweathermap.org/data/2.5/forecast?q="
            f"{place}&units=metric&appid={API_KEY}")
-
     response = requests.get(url)
     data = response.json()
-
+    filtered_data = data["list"]
     nr_values = 8 * int(days)
-    filtered_data = data['list'][:nr_values]
+    filtered_data = filtered_data[:nr_values]
 
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
     return filtered_data
 
-
 if __name__ == "__main__":
-    print(get_data("Tokyo", '2', "Sky"))
-
-
+    print('working')
 
 
